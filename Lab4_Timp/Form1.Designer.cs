b12b567ea4e5a1f6200f50b2874b3c2de87ba844
Каракулло Лев <CrossoutGamer226@gmail.com>
2026-05-07 20:05:24 +0600

    Добавьте файлы проекта.

---
+++ Lab4_Timp/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Lab4_Timp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listFiles = new ListBox();
+            groupClient = new GroupBox();
+            txtClientLog = new TextBox();
+            groupServer = new GroupBox();
+            txtServerLog = new TextBox();
+            btnSendToServer = new Button();
+            btnSendToClient = new Button();
+            lblIp = new Label();
+            txtIp = new TextBox();
+            btnDisconnect = new Button();
+            btnServerDisconnect = new Button();
+            btnExit = new Button();
+            cmbPath = new ComboBox();
+            groupClient.SuspendLayout();
+            groupServer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listFiles
+            // 
+            listFiles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            listFiles.FormattingEnabled = true;
+            listFiles.ItemHeight = 15;
+            listFiles.Location = new Point(10, 45);
+            listFiles.Name = "listFiles";
+            listFiles.Size = new Size(320, 454);
+            listFiles.TabIndex = 1;
+            listFiles.SelectedIndexChanged += listFiles_SelectedIndexChanged;
+            // 
+            // groupClient
+            // 
+            groupClient.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            groupClient.Controls.Add(txtClientLog);
+            groupClient.Location = new Point(340, 10);
+            groupClient.Name = "groupClient";
+            groupClient.Size = new Size(300, 625);
+            groupClient.TabIndex = 2;
+            groupClient.TabStop = false;
+            groupClient.Text = "Клиентская сторона";
+            groupClient.Enter += groupClient_Enter;
+            // 
+            // txtClientLog
+            // 
+            txtClientLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtClientLog.Location = new Point(10, 20);
+            txtClientLog.Multiline = true;
+            txtClientLog.Name = "txtClientLog";
+            txtClientLog.ReadOnly = true;
+            txtClientLog.ScrollBars = ScrollBars.Vertical;
+            txtClientLog.Size = new Size(280, 595);
+            txtClientLog.TabIndex = 0;
+            txtClientLog.TextChanged += txtClientLog_TextChanged;
+            // 
+            // groupServer
+            // 
+            groupServer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            groupServer.Controls.Add(txtServerLog);
+            groupServer.Location = new Point(650, 10);
+            groupServer.Name = "groupServer";
+            groupServer.Size = new Size(300, 625);
+            groupServer.TabIndex = 1;
+            groupServer.TabStop = false;
+            groupServer.Text = "Серверная сторона";
+            groupServer.Enter += groupServer_Enter;
+            // 
+            // txtServerLog
+            // 
+            txtServerLog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtServerLog.Location = new Point(10, 20);
+            txtServerLog.Multiline = true;
+            txtServerLog.Name = "txtServerLog";
+            txtServerLog.ReadOnly = true;
+            txtServerLog.ScrollBars = ScrollBars.Vertical;
+            txtServerLog.Size = new Size(280, 595);
+            txtServerLog.TabIndex = 0;
+            txtServerLog.TextChanged += txtServerLog_TextChanged;
+            // 
+            // btnSendToServer
+            // 
+            btnSendToServer.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnSendToServer.Location = new Point(10, 605);
+            btnSendToServer.Name = "btnSendToServer";
+            btnSendToServer.Size = new Size(130, 30);
+            btnSendToServer.TabIndex = 1;
+            btnSendToServer.Text = "Передать серверу";
+            btnSendToServer.UseVisualStyleBackColor = true;
+            btnSendToServer.Click += btnSendToServer_Click;
+            // 
+            // btnSendToClient
+            // 
+            btnSendToClient.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnSendToClient.Location = new Point(200, 605);
+            btnSendToClient.Name = "btnSendToClient";
+            btnSendToClient.Size = new Size(130, 30);
+            btnSendToClient.TabIndex = 3;
+            btnSendToClient.Text = "Передать клиенту";
+            btnSendToClient.UseVisualStyleBackColor = true;
+            btnSendToClient.Click += btnSendToClient_Click;
+            // 
+            // lblIp
+            // 
+            lblIp.AutoSize = true;
+            lblIp.Location = new Point(10, 508);
+            lblIp.Name = "lblIp";
+            lblIp.Size = new Size(53, 15);
+            lblIp.TabIndex = 4;
+            lblIp.Text = "IP-адрес";
+            lblIp.Click += lblIp_Click;
+            // 
+            // txtIp
+            // 
+            txtIp.Location = new Point(69, 508);
+            txtIp.Name = "txtIp";
+            txtIp.Size = new Size(120, 23);
+            txtIp.TabIndex = 5;
+            txtIp.Text = "127.0.0.1";
+            txtIp.TextChanged += txtIp_TextChanged;
+            // 
+            // btnDisconnect
+            // 
+            btnDisconnect.Location = new Point(12, 543);
+            btnDisconnect.Name = "btnDisconnect";
+            btnDisconnect.Size = new Size(110, 30);
+            btnDisconnect.TabIndex = 6;
+            btnDisconnect.Text = "Отключиться";
+            btnDisconnect.UseVisualStyleBackColor = true;
+            btnDisconnect.Click += btnDisconnect_Click;
+            // 
+            // btnServerDisconnect
+            // 
+            btnServerDisconnect.Location = new Point(200, 507);
+            btnServerDisconnect.Name = "btnServerDisconnect";
+            btnServerDisconnect.Size = new Size(130, 30);
+            btnServerDisconnect.TabIndex = 7;
+            btnServerDisconnect.Text = "Сервер отключить";
+            btnServerDisconnect.UseVisualStyleBackColor = true;
+            btnServerDisconnect.Click += btnServerDisconnect_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnExit.Location = new Point(230, 543);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(100, 30);
+            btnExit.TabIndex = 8;
+            btnExit.Text = "Выход";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // cmbPath
+            // 
+            cmbPath.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbPath.FormattingEnabled = true;
+            cmbPath.Location = new Point(10, 10);
+            cmbPath.Name = "cmbPath";
+            cmbPath.Size = new Size(320, 23);
+            cmbPath.TabIndex = 9;
+            cmbPath.SelectedIndexChanged += cmbPath_SelectedIndexChanged;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(984, 647);
+            Controls.Add(cmbPath);
+            Controls.Add(btnExit);
+            Controls.Add(btnServerDisconnect);
+            Controls.Add(btnDisconnect);
+            Controls.Add(txtIp);
+            Controls.Add(lblIp);
+            Controls.Add(btnSendToClient);
+            Controls.Add(btnSendToServer);
+            Controls.Add(groupServer);
+            Controls.Add(groupClient);
+            Controls.Add(listFiles);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Программа для обмена данными между компьютерами";
+            groupClient.ResumeLayout(false);
+            groupClient.PerformLayout();
+            groupServer.ResumeLayout(false);
+            groupServer.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ListBox listFiles;
+        private GroupBox groupClient;
+        private TextBox txtClientLog;
+        private GroupBox groupServer;
+        private TextBox txtServerLog;
+        private Button btnSendToServer;
+        private Button btnSendToClient;
+        private Label lblIp;
+        private TextBox txtIp;
+        private Button btnDisconnect;
+        private Button btnServerDisconnect;
+        private Button btnExit;
+        private ComboBox cmbPath;
+    }
+}
---
+++ Lab4_Timp/Form1.cs
@@ -0,0 +1,294 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Lab4_Timp
+{
+    public partial class MainForm : Form
+    {
+        // Клиент
+        private TcpClient? client;
+        private NetworkStream? clientStream;
+
+        // Сервер
+        private TcpListener? server;
+        private NetworkStream? serverStream;
+        private bool serverRunning = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadDrives();
+            StartServer();
+        }
+
+        // ============================================================
+        // ===============   ЗАГРУЗКА ДИСКОВ И ФАЙЛОВ   ===============
+        // ============================================================
+
+        private void LoadDrives()
+        {
+            cmbPath.Items.Clear();
+
+            foreach (string drive in Directory.GetLogicalDrives())
+                cmbPath.Items.Add(drive);
+
+            if (cmbPath.Items.Count > 0)
+                cmbPath.SelectedIndex = 0;
+        }
+
+        private void LoadFiles(string path)
+        {
+            try
+            {
+                listFiles.Items.Clear();
+
+                string[] dirs = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+
+                foreach (string d in dirs)
+                    listFiles.Items.Add(d);
+
+                foreach (string f in files)
+                    listFiles.Items.Add(f);
+            }
+            catch (Exception ex)
+            {
+                txtClientLog.AppendText($"Ошибка загрузки файлов: {ex.Message}\r\n");
+            }
+        }
+
+        private void cmbPath_SelectedIndexChanged(object? sender, EventArgs e)
+        {
+            string? path = cmbPath.SelectedItem as string;
+            if (path != null)
+                LoadFiles(path);
+        }
+
+        private void listFiles_DoubleClick(object? sender, EventArgs e)
+        {
+            if (listFiles.SelectedItem is not string path)
+                return;
+
+            if (Directory.Exists(path))
+            {
+                cmbPath.Text = path;
+                LoadFiles(path);
+            }
+        }
+
+        // ============================================================
+        // ===============   СЕРВЕРНАЯ ЧАСТЬ   =========================
+        // ============================================================
+
+        private void StartServer()
+        {
+            try
+            {
+                server = new TcpListener(IPAddress.Any, 9000);
+                server.Start();
+                serverRunning = true;
+
+                txtServerLog.AppendText($"Сервер включён {DateTime.Now}\r\n");
+
+                server.BeginAcceptTcpClient(OnClientConnected, null);
+            }
+            catch (Exception ex)
+            {
+                txtServerLog.AppendText($"Ошибка запуска сервера: {ex.Message}\r\n");
+            }
+        }
+
+        private void OnClientConnected(IAsyncResult ar)
+        {
+            try
+            {
+                if (server == null) return;
+
+                TcpClient connectedClient = server.EndAcceptTcpClient(ar);
+                serverStream = connectedClient.GetStream();
+
+                Invoke(new Action(() =>
+                {
+                    txtServerLog.AppendText($"Клиент подключился {DateTime.Now}\r\n");
+                }));
+
+                string drives = string.Join(",", Directory.GetLogicalDrives());
+                byte[] data = Encoding.UTF8.GetBytes(drives);
+                serverStream.Write(data, 0, data.Length);
+
+                byte[] buffer = new byte[4096];
+                serverStream.BeginRead(buffer, 0, buffer.Length, OnServerDataReceived, buffer);
+
+                server.BeginAcceptTcpClient(OnClientConnected, null);
+            }
+            catch { }
+        }
+
+        private void OnServerDataReceived(IAsyncResult ar)
+        {
+            try
+            {
+                if (serverStream == null) return;
+
+                byte[]? buffer = ar.AsyncState as byte[];
+                if (buffer == null) return;
+
+                int bytes = serverStream.EndRead(ar);
+                if (bytes <= 0) return;
+
+                string received = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                Invoke(new Action(() =>
+                {
+                    txtServerLog.AppendText($"Сервер получил: {received}\r\n");
+                }));
+
+                // === КЛИЕНТ ЗАПРОСИЛ ОТКЛЮЧЕНИЕ ===
+                if (received == "exit")
+                {
+                    Invoke(new Action(() =>
+                    {
+                        txtServerLog.AppendText("Клиент запросил отключение.\r\n");
+                    }));
+
+                    serverStream.Close();
+                    serverStream = null;
+                    return;
+                }
+
+                // === КАТАЛОГ ===
+                if (Directory.Exists(received))
+                {
+                    string[] items = Directory.GetFileSystemEntries(received);
+                    string response = string.Join(",", items);
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+                    serverStream.Write(data, 0, data.Length);
+                }
+                // === ФАЙЛ ===
+                else if (File.Exists(received))
+                {
+                    string text = File.ReadAllText(received);
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+                    serverStream.Write(data, 0, data.Length);
+                }
+
+                serverStream.BeginRead(buffer, 0, buffer.Length, OnServerDataReceived, buffer);
+            }
+            catch { }
+        }
+
+        private void btnServerDisconnect_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                serverRunning = false;
+                server?.Stop();
+                txtServerLog.AppendText($"Сервер отключён {DateTime.Now}\r\n");
+            }
+            catch { }
+        }
+
+        // ============================================================
+        // ===============   КЛИЕНТСКАЯ ЧАСТЬ   ========================
+        // ============================================================
+
+        private void ConnectClient()
+        {
+            if (client != null && client.Connected) return;
+
+            client = new TcpClient();
+            client.Connect(txtIp.Text, 9000);
+
+            clientStream = client.GetStream();
+
+            txtClientLog.AppendText($"Клиент подключён {DateTime.Now}\r\n");
+
+            byte[] buffer = new byte[4096];
+            clientStream.BeginRead(buffer, 0, buffer.Length, OnClientDataReceived, buffer);
+        }
+
+        private void OnClientDataReceived(IAsyncResult ar)
+        {
+            try
+            {
+                if (clientStream == null) return;
+
+                byte[]? buffer = ar.AsyncState as byte[];
+                if (buffer == null) return;
+
+                int bytes = clientStream.EndRead(ar);
+                if (bytes <= 0) return;
+
+                string received = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+                Invoke(new Action(() =>
+                {
+                    txtClientLog.AppendText($"Клиент получил: {received}\r\n");
+                }));
+
+                clientStream.BeginRead(buffer, 0, buffer.Length, OnClientDataReceived, buffer);
+            }
+            catch { }
+        }
+
+        private void btnSendToServer_Click(object? sender, EventArgs e)
+        {
+            if (listFiles.SelectedItem is not string path)
+            {
+                txtClientLog.AppendText("Не выбран файл или каталог.\r\n");
+                return;
+            }
+
+            ConnectClient();
+
+            byte[] data = Encoding.UTF8.GetBytes(path);
+            clientStream?.Write(data, 0, data.Length);
+
+            txtClientLog.AppendText($"Отправлено серверу: {path}\r\n");
+        }
+
+        private void btnSendToClient_Click(object? sender, EventArgs e)
+        {
+            txtClientLog.AppendText("Кнопка 'Передать клиенту' нажата (функция не реализована).\r\n");
+        }
+
+        private void btnDisconnect_Click(object? sender, EventArgs e)
+        {
+            try
+            {
+                if (clientStream != null)
+                {
+                    byte[] exitMsg = Encoding.UTF8.GetBytes("exit");
+                    clientStream.Write(exitMsg, 0, exitMsg.Length);
+                }
+
+                client?.Close();
+                client = null;
+
+                txtClientLog.AppendText($"Клиент отключён {DateTime.Now}\r\n");
+            }
+            catch { }
+        }
+
+        private void btnExit_Click(object? sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // ============================================================
+        // ===============   ПУСТЫЕ ОБРАБОТЧИКИ   ======================
+        // ============================================================
+
+        private void listFiles_SelectedIndexChanged(object? sender, EventArgs e) { }
+        private void groupClient_Enter(object? sender, EventArgs e) { }
+        private void txtClientLog_TextChanged(object? sender, EventArgs e) { }
+        private void groupServer_Enter(object? sender, EventArgs e) { }
+        private void txtServerLog_TextChanged(object? sender, EventArgs e) { }
+        private void txtIp_TextChanged(object? sender, EventArgs e) { }
+        private void lblIp_Click(object? sender, EventArgs e) { }
+    }
+}
